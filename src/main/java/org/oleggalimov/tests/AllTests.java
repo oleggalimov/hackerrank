3fdd992f0ec1d493673ed7a5a6351ef25a261919
oleggalimov <o.d.galimov>
2019-08-12 14:55:23 +0500

    solved: Java Currency Formatter

---
+++ src/main/java/org/oleggalimov/tests/AllTests.java
@@ -0,0 +1,61 @@
+package main.java.org.oleggalimov.tests;
+
+
+import main.java.org.oleggalimov.tasks.JavaCurrencyFormatter;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+
+public class AllTests {
+    private ByteArrayOutputStream byteArrayOutputStream;
+    private PrintStream console;
+
+    @Before
+    public void setup() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        console = System.out;
+    }
+
+    @Test
+    public void javaCurrencyFormatter() throws Exception {
+        double data = 12324.134D;
+        runTest(String.valueOf(data), JavaCurrencyFormatter.class.getName());
+        Assert.assertEquals(
+                "US: $12,324.13\n" +
+                "India: Rs.12,324.13\n" +
+                "China: ￥12,324.13\n" +
+                "France: 12 324,13 €\n",
+
+                byteArrayOutputStream.toString()
+        );
+    }
+
+    private void runTest(final String data, final String className) throws Exception {
+
+        final InputStream input = new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8));
+        final InputStream old = System.in;
+
+        try {
+            System.setOut(new PrintStream(byteArrayOutputStream));
+            System.setIn(input);
+
+            final Class<?> cls = Class.forName(className);
+            final Method meth = cls.getDeclaredMethod("main", String[].class);
+            final String[] params = new String[]{};
+            meth.invoke(null, (Object) params);
+
+        } finally {
+            System.setOut(console);
+            System.setIn(old);
+        }
+    }
+
+}
